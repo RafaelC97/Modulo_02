260d893d907a948313efeec29e74d92d6c74f9a0
RafaelC97 <rafael.castro97@hotmail.com>
2023-04-06 19:23:07 -0300

    Listas de Exercicios

---
+++ Desenvolvimento_Web/Listas/Lista_01/js/exercicio_02.js
@@ -0,0 +1,17 @@
+var nome1 = "Miguel"
+var nome2 = "Carlos"
+var nome3 = "Ana"
+var nome4 = "José"
+var nome5 = "Marcia"
+
+var idade1 = 22
+var idade2 = 13
+var idade3 = 49
+var idade4 = 37
+var idade5 = 71
+
+console.log(nome1, " - ", idade1, " anos")
+console.log(nome2, " - ", idade2, " anos")
+console.log(nome3, " - ", idade3, " anos")
+console.log(nome4, " - ", idade4, " anos")
+console.log(nome5, " - ", idade5, " anos")
